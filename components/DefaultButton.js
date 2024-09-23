1eb253ca3f2e90c611a42f0dd431278716594b61
iuraShock <diurasoc@gmail.com>
2024-09-23 22:36:23 +0300

    Added homepage content and custom button component

---
+++ components/DefaultButton.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+const DefaultButton = (props) => {
+  const styles = myStyles(props);
+  return (
+    <View style={styles.container}>
+      <Pressable
+        onPress={props.action}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? props.pressedColor : props.color,
+          },
+          styles.centered,
+        ]}
+      >
+        <Text style={styles.title}> {props.title} </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const myStyles = (props) =>
+  StyleSheet.create({
+    container: {
+      //justifyContent: 'center',
+      //alignItems: 'center',
+      //borderRadius: 9,
+    },
+
+    centered: {
+      width: props.width,
+      height: props.height,
+      justifyContent: 'center',
+      alignItems: 'center',
+      overflow: 'hidden',
+      borderRadius: 9,
+    },
+
+    title: {
+      fontSize: props.fontSize,
+      color: props.fontColor,
+    },
+  });
+
+export default DefaultButton;
